5e79e95b01d708fde1bff433e1135314a30e7031
Haniifah <92525360+haniifahfawziyyah@users.noreply.github.com>
2022-07-19 13:57:42 +0700

    Delete GitHubUser2 directory

--- GitHubUser2/app/src/main/java/com/haniifah/submission2/githubuser/data/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.haniifah.submission2.githubuser.data.model
-
-data class User(
-    val login: String,
-    val id: Int,
-    val avatar_url: String,
-    val name: String,
-    val incomplete_result: Boolean
-)
--- GitHubUser2/app/src/main/java/com/haniifah/submission2/githubuser/data/model/UserResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.haniifah.submission2.githubuser.data.model
-
-data class UserResponse(
-    var items: ArrayList<User>,
-    val incomplete_result: Boolean
-)
